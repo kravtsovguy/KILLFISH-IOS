3417418d3ec4a197421355c8f5b645fa390b5b2c
Матвей Кравцов <matvey@macbook-pro-matvej.local>
2016-01-28 15:36:42 +0300

    Initial Commit

---
+++ KILLFISH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KILLFISH
+//
+//  Created by Матвей Кравцов on 28.01.16.
+//  Copyright © 2016 Матвей Кравцов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
